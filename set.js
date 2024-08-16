f948834d76cba1ecdcfa0b5a1abea472d2b9f919
Ton281 <clintoonchari29@gmail.com>
2024-08-16 23:59:47 +0300

    Update set.js

--- set.js
+++ set.js
@@ -9,8 +9,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "keithkeizzah",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " keithkeizzah",              
+    OWNER_NAME: process.env.OWNER_NAME || "clinton",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || " clinton",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
     BOT : process.env.BOT_NAME || '𝐀𝐋𝐏𝐇𝐀-𝐌𝐃',
